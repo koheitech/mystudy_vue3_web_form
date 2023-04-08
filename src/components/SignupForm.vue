16cbc108fc7041b072eef0671dcf353102caafe1
koheitech <koheitechigh@gmail.com>
2023-04-08 08:52:06 +0300

    v-model for two-way binding

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,13 +1,22 @@
 <template>
   <form>
-    <label>EMAIL: </label>
-    <input type="email" required>
+    <label>Email: </label>
+    <input type="email" required v-model="email">
+    <label>Password: </label>
+    <input type="password" required v-model="password">
   </form>
+  <p>Email: {{ email }}</p>
+  <p>Password: {{ password }}</p>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    }
 }
 </script>
 
