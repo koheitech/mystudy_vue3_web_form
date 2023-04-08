a21347837f5157760eaf20e3c7f5545e80ae7f8d
koheitech <koheitechigh@gmail.com>
2023-04-08 08:56:27 +0300

    add select box with v-model

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -2,11 +2,19 @@
   <form>
     <label>Email: </label>
     <input type="email" required v-model="email">
+    
     <label>Password: </label>
     <input type="password" required v-model="password">
+
+    <label>Role: </label>
+    <select v-model="role">
+        <option value="developer">Web Developer</option>
+        <option value="designer">Web Designer</option>
+    </select>
   </form>
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
+  <p>Role: {{ role }}</p>
 </template>
 
 <script>
@@ -14,7 +22,8 @@ export default {
     data() {
         return {
             email: '',
-            password: ''
+            password: '',
+            role: 'designer'
         }
     }
 }
@@ -38,7 +47,7 @@ export default {
     letter-spacing: 1px;
     font-weight: bold;
   }
-  input {
+  input, select {
     display: block;
     padding: 10px 6px;
     width: 100%;
