0f3d80add5ed4e9d5581d7f97372294e37fdeaf2
koheitech <koheitechigh@gmail.com>
2023-04-08 08:45:59 +0300

    add signup form component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <h1>Form</h1>
+  <SignupForm />
 </template>
 
 <script>
+import SignupForm from './components/SignupForm.vue'
 
 export default {
   name: 'App',
   components: {
+    SignupForm
   }
 }
 </script>
@@ -19,5 +21,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  margin: 0;
+  background: #eee;
 }
 </style>
